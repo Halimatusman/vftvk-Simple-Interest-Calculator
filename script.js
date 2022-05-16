7a718760ce864a05b5ec92bab10d39a44e1b9d2e
Halimatusman <103938155+Halimatusman@users.noreply.github.com>
2022-05-16 11:07:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function compute()
 {
+var principal = document.getElementById("principal").value;
+if(parseInt(principal) < 1){
+    alert("Enter a positive number");
+    document.getElementById("principal"). focus ();
+    return;
+    }
     p = document.getElementById("principal").value;
     
 }
